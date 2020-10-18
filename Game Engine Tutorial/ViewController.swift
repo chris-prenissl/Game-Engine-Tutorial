63a04f8d894197f6ae42367e6b8782755e378ff1
snakept <snakept@icloud.com>
2020-10-18 14:22:19 +0200

    Initial Commit

---
+++ Game Engine Tutorial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game Engine Tutorial
+//
+//  Created by Christoph Prenissl on 18.10.20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
