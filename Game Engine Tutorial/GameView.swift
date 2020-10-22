390f972f528477b69397a627a65a92f27414c08d
snakept <snakept@icloud.com>
2020-10-22 18:05:04 +0200

    draw white triangle

--- Game Engine Tutorial/GameView.swift
+++ Game Engine Tutorial/GameView.swift
@@ -1,9 +1,68 @@
 import MetalKit
 
+public typealias float3 = SIMD3<Float>
+
 class GameView: MTKView {
+    
+    var commandQueue : MTLCommandQueue!
+    var renderPipelineState : MTLRenderPipelineState!
+    
+    let vertices: [float3] = [
+        float3(0, 1, 0),
+        float3(-1,-1,0),
+        float3(1,-1,0)
+    ]
+    
+    var vertexBuffer: MTLBuffer!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
         
+        self.device = MTLCreateSystemDefaultDevice()
+        self.clearColor = MTLClearColor(red: 0.1, green: 0.3, blue: 0.92, alpha: 1.0)
+        self.colorPixelFormat = .bgra8Unorm
+        self.commandQueue = device?.makeCommandQueue()
+        
+        createRenderPipelineState()
+        createBuffers()
+    }
+    
+    func createRenderPipelineState() {
+        let library = device?.makeDefaultLibrary()
+        let vertexFunction = library?.makeFunction(name: "basic_vertex_shader")
+        let fragmentFunction = library?.makeFunction(name: "basic_fragment_shader")
+        
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineDescriptor.vertexFunction = vertexFunction
+        renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        
+        do {
+            renderPipelineState = try device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    func createBuffers() {
+        vertexBuffer = device?.makeBuffer(bytes: vertices, length: MemoryLayout<float3>.stride * vertices.count * 2, options: [])
+        
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderCommandEncoder?.setRenderPipelineState(renderPipelineState)
+        
+        renderCommandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderCommandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
+        
+        
+        
+        renderCommandEncoder?.endEncoding()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
     }
 }
