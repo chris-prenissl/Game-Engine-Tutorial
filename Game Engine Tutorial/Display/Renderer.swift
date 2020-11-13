b9fc1b2f14d898ec330574c29e7420258e623555
snakept <snakept@icloud.com>
2020-11-13 22:08:19 +0100

    translate, scale and rotate for gameobject now possible

--- Game Engine Tutorial/Display/Renderer.swift
+++ Game Engine Tutorial/Display/Renderer.swift
@@ -25,6 +25,8 @@ extension Renderer: MTKViewDelegate {
         let commandBuffer = Engine.CommandQueue.makeCommandBuffer()
         let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
         
+        player.update(deltaTime: 1 / Float(view.preferredFramesPerSecond))
+        
         player.render(renderCommandEncoder: renderCommandEncoder!)
         
         renderCommandEncoder?.endEncoding()
